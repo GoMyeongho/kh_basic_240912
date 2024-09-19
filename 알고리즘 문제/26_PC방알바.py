518f9b0b95efc66b2ad8dea9de32b3a390f55e2e
GoMyeongho <sdx02013@ajou.ac.kr>
2024-09-19 14:50:06 +0900

    240919 수업 5 교시

---
+++ 알고리즘 문제/26_PC방알바.py
@@ -0,0 +1,39 @@
+customer_num = int(input())
+num_list = list(map(int,input().split()))
+
+num_set = set(num_list)
+
+print(len(num_list) - len(num_set))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
